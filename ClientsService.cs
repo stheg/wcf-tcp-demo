906a3d6150f783bcce08ef1bc3f52c74ce5afc19
Sergey Pavlov <serega--pavlov@yandex.ru>
2016-05-05 17:35:46 +0300

    wcf tcp service demo project. add and get clients information

---
+++ ClientsService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfTcpDemo
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ClientsService : IClientCollectionService
+    {
+        private List<Client> _clients = new List<Client>();
+        private Dictionary<string, List<string>> _additionalClientInfo = new Dictionary<string, List<string>>();
+
+        public bool AddClient(string clientName)
+        {
+            if (String.IsNullOrEmpty(clientName)) throw new ArgumentNullException();
+
+            if (!_clients.Exists(c => c.Name == clientName))
+            {
+                Client newClient = new Client() { Name = clientName, LastChanges = DateTime.Now };
+                _clients.Add(newClient);
+
+                var defaultClientInfo = new List<string>() { clientName };
+                _additionalClientInfo.Add(clientName, defaultClientInfo);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool AddInfoToClient(string clientName, string additionalInfo)
+        {
+            if (String.IsNullOrEmpty(clientName) || String.IsNullOrEmpty(additionalInfo))
+                throw new ArgumentNullException();
+
+            if (_additionalClientInfo.ContainsKey(clientName))
+            {
+                _additionalClientInfo[clientName].Add(additionalInfo);
+
+                var client = _clients.Find(c => c.Name == clientName);
+                client.LastChanges = DateTime.Now;
+
+                return true;
+            }
+            
+            return false;
+        }
+
+        public Client GetClient(string clientName)
+        {
+            if (String.IsNullOrEmpty(clientName)) throw new ArgumentNullException();
+
+            return _clients.Find(c => c.Name == clientName);
+        }
+
+        public bool RemoveClient(string clientName)
+        {
+            if (String.IsNullOrEmpty(clientName)) throw new ArgumentNullException();
+
+            Client client;
+            if ((client = _clients.Find(c => c.Name == clientName)) != null)
+            {
+                _clients.Remove(client);
+
+                _additionalClientInfo[clientName].Clear();
+                _additionalClientInfo.Remove(clientName);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public List<string> GetClientInfo(string clientName)
+        {
+            if (String.IsNullOrEmpty(clientName)) throw new ArgumentNullException();
+
+            return _additionalClientInfo[clientName];
+        }
+    }
+}
---
+++ IClientCollectionService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfTcpDemo
+{
+    [ServiceContract(ProtectionLevel = System.Net.Security.ProtectionLevel.None)]
+    public interface IClientCollectionService
+    {
+        [OperationContract]
+        bool AddClient(string clientName);
+
+        [OperationContract]
+        bool RemoveClient(string clientName);
+
+        [OperationContract]
+        bool AddInfoToClient(string clientName, string additionalInfo);
+
+        [OperationContract]
+        Client GetClient(string clientName);
+
+        [OperationContract]
+        List<string> GetClientInfo(string clientName);
+    }
+
+    [DataContract]
+    public class Client
+    {
+        string _name;
+
+        [DataMember]
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        [DataMember]
+        public DateTime LastChanges
+        {
+            get
+            {
+                return _lastChanges;
+            }
+
+            set
+            {
+                _lastChanges = value;
+            }
+        }
+
+        DateTime _lastChanges;
+    }
+}
